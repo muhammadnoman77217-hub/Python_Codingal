19be054f7a5fd6b03fd670c7deed3b52c3a2f201
muhammadnoman77217-hub <muhammadnoman77217@gmail.com>
2026-06-18 19:48:51 +0300

    activity_1.py

---
+++ lesson_3/activity_1.py
@@ -0,0 +1,10 @@
+a = 123
+b = "Hello"
+c = 10.2
+d = True
+
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c is", type(c))
+print("the type of d is", type(d))
+
