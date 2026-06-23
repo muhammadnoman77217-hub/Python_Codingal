505d542698987b2f3e6f4ad09f5c739566d75903
muhammadnoman77217-hub <muhammadnoman77217@gmail.com>
2026-06-23 19:44:27 +0300

    activity_1.py

---
+++ lesson_4/activity_1.py
@@ -0,0 +1,11 @@
+a=10
+b=3
+
+print(a+b)      #addition
+print(a-b)      #subtraction
+print(a*b)      #multiplicatio
+print(a/b)      #division
+
+print(a%b)      #Reminder or modulus
+print(a**b)     #exponent
+print(a//b)     #floot
