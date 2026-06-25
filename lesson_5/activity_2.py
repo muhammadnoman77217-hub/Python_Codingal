79c746e9d94138dd929ac80de77d4e91c10f90f3
muhammadnoman77217-hub <muhammadnoman77217@gmail.com>
2026-06-25 19:54:55 +0300

    activity_2.py

---
+++ lesson_5/activity_2.py
@@ -0,0 +1,5 @@
+num = int(input("enter a number to check if its positive or negative : "))
+if num > 0:
+    print(num," this is a positive number") 
+else:
+    print(num," this is a negative number")
