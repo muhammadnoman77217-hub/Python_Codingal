30ea73cc0a98f35f9822874a4160e9c36e212b28
muhammadnoman77217-hub <muhammadnoman77217@gmail.com>
2026-08-25 19:55:50 +0500

    activity_1.py

---
+++ lesson_19/activity_1.py
@@ -0,0 +1,15 @@
+tup1 = ("name","5",True,50)
+print(tup1)
+
+tup2=(4,6,2,8,3,1)
+print(tup2)
+
+tup2 = tup2 + ("hi",)
+print(tup2)
+
+tuple1 = (50, 10, 70, 60, 50)
+print(tuple1.count(50))
+
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+slc = tuplex[3:5]
+print(slc)
